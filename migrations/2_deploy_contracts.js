99f5cbe5bce62124cec1cda7b92da42bfc8f8e29
Aaryamann Challani <aaryamannchallani7@gmail.com>
2020-12-05 17:53:03 +0000

    feat: add deployment scripts [wip]

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,22 @@
+const HTLC = artifacts.require('HTLC');
+const ethers = require('ethers');
+
+module.exports = async function(deployer, network, addresses) {
+  const [a1, a2] = addresses;
+
+  if (network === 'kovan') {
+    // sUSD
+    await deployer.deploy(HTLC, a2, '0xF92e70301E26AaDeCCEc5016b7D0167DAF416d72', ethers.utils.formatEther('5'), {from: a1});
+    const htlc = await htlc.deployed();
+    const token = new ethers.Contract('0xF92e70301E26AaDeCCEc5016b7D0167DAF416d72', abi, provider);
+    await token.approve(htlc.address, ethers.utils.formatEther('5'));
+    await htlc.fund({from: a1});
+  } else if(network === 'binanceTestnet') {
+    // DAI
+    await deployer.deploy(HTLC, a1, '0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867', ethers.utils.formatEther('5'), {from: a2});
+    const htlc = await htlc.deployed();
+    const token = new ethers.Contract('0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867', abi, provider);
+    await token.approve(htlc.address, ethers.utils.formatEther('5'));
+    await htlc.fund({from: a2});
+  }
+}
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -18,11 +19,27 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+const kovanProvider = new HDWalletProvider({
+  privateKeys: [
+    process.env.a1,
+    process.env.a2,
+  ],
+  providerOrUrl: process.env.KOVAN_URL
+});
+
+const bscProvider = new HDWalletProvider({
+  privateKeys: [
+    process.env.a1,
+    process.env.a2,
+  ],
+  providerOrUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545'
+})
 
 module.exports = {
   /**
@@ -36,47 +53,17 @@ module.exports = {
    */
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
-  },
+     kovan: {
+       provider: () => kovanProvider,
+       network_id: "42",
+       gas: 200000
+     },
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
+     binanceTestnet: {
+       provider: () => bscProvider,
+       network_id: "97",
+       gas: 200000
+     }
   },
 
   // Configure your compilers
